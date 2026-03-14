2052a4a1bba9b5cd7fa6198b9c023844f38cf69d
Evaevita <ruthmena6917@gmail.com>
2026-03-14 13:56:41 -0500

    Agrega sistema de recuperación de contraseña

--- MoonManga/guardar.php
+++ MoonManga/guardar.php
@@ -1,14 +1,22 @@
-<?php 
+<?php
+include 'conexion.php';
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$password = password_hash ($_POST['password'], PASSWORD_DEFAULT);
+
+$verificar = mysqli_query($conexion, "SELECT * FROM usuarios WHERE email='$email'");
+
+if(mysqli_num_rows($verificar)> 0){
+    echo "Este correo ya esta registrado inicie sesion o use otro correo";
+    exit(); 
+    
+    }
 
 
+    $query = "INSERT INTO usuarios (nombre, email, password) VALUES ('$nombre', '$email', '$password')";
 
+    mysqli_query( $conexio, $query);
 
-
-
-
-
-
-
-
-
+    echo "Usuario registrado";
 ?>
