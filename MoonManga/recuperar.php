1c9a249fd0595619f9ea6cfbd38ce064a18f4c83
Evaevita <ruthmena6917@gmail.com>
2026-03-16 14:56:23 -0500

    con css de recuperar

--- MoonManga/recuperar.php
+++ MoonManga/recuperar.php
@@ -3,19 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Recuperar contraseña</title>   <link rel="stylesheet" href="../css/recuperar.css">
 </head>
-<body>
 
-    <form action="enviar_correo.php" method="POST">
-
-        <label>Ingrese su correo</label>
-        <input type="email" name="email" required>
-
-        <button type="submit">Recuperar contraseña</button>
-
-    </form>
     
 </body>
 </html>
+        <Form action="enviar_correo.php" class="web-R" method="POST">
+        <div class="box-R">
+            <div class="comp-">
+            <h1>Recupera tu contraseña con MoonManga</h1>
+            </div>
+            <div class="R-R">
+            <form action="enviar_correo.php" method="POST">
+                <label class="label">Ingrese su correo</label>
+                <input type="email" name="email" required class="email">
+                <button type="submit" class="btn-R">Recuperar contraseña</button>
+
+            </form>
+            </div>
+        </div>
+</body>
 
