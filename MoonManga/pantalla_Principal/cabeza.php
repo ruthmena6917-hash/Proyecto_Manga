865f8aec9e46c59555512af90a2d7bc9dcf0639b
Evaevita <ruthmena6917@gmail.com>
2026-03-16 15:41:43 -0500

    cambio

---
+++ MoonManga/pantalla_Principal/cabeza.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="../../img/icon-logo.png">
+    <link rel="stylesheet" href="../../css/cabeza.css">
+
+</head>
+<body>
+    <header>
+        <div class="box-header">
+                <nav class="heade_P">
+                    <a href="../panel.php"><img src="../../img/icon-logo.png" alt="icono de MoonManga" class="ico-logo"></a>
+                      <h1>MoonManga</h1>
+                     <ul class="menu">
+                            <li>
+                                
+                                <a href="#">Nuevos</a>
+                                <a href="#">Populares</a>
+                                <a href="#">Géneros</a>
+                                <ul class="submenu">
+                                    <li><a href="yaoi.php">Yaoi +18</a></li>
+                                    <li><a href="accion.php">Acción</a></li>
+                                    <li><a href="romance.php">Romance</a></li>
+                                    <li><a href="aventura.php">Aventura</a></li>
+                                    <li><a href="fantasia.php">Fantasía</a></li>
+                                    <li><a href="comedia.php">Comedia</a></li>
+                                    <li><a href="drama.php">Drama</a></li>
+                                    <li><a href="terror.php">Terror</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    
+                </nav>
+        </div> 
+    </header>
+    
+</body>
+</html>
